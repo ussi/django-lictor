6ed134d4d9c968c04ca795d0943b387b44621d63
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2012-08-18 17:07:19 +0400

    Added collector.Trace, rename and update middleware for collection information

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -98,7 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'lictor.middleware.Tracer',
+    'lictor.middleware.CollectMiddleware',
 )
 
 ROOT_URLCONF = 'test_project.urls'
