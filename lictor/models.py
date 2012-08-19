960bd0b0af024132eb226c9032bec15aaf8a96da
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2012-08-19 20:47:43 +0400

    Convert the Trace.session field from PositiveIntegerField to BigIntegerField

--- lictor/models.py
+++ lictor/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Trace(models.Model):
     created = models.DateTimeField(auto_now_add=True)
-    session = models.PositiveIntegerField()
+    session = models.BigIntegerField()
     json = models.TextField()
