f71526ed55e7b3352065a180797c8f757813e4c7
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2012-08-19 13:44:26 +0400

    Replace middleware to lictor.activate() in settings, added replacer for monkey-patching django get_response(), added Collector.Graph (for building call graph in thread and save result to database), Collector.FrameInspector for extract additional information from a frame

---
+++ lictor/fake.py
@@ -0,0 +1,21 @@
+import hashlib
+import simplejson
+
+from lictor.models import Trace
+
+
+def md5(s):
+    return hashlib.md5(s).hexdigest()
+
+def make_fake(request):
+    session = request.COOKIES.get(settings.LICTOR_SESSION_COOKIE_NAME)
+    dump = [
+        {"i": md5("test_project.films.urls:12"), "t": "Url", "f": "test_project.films.urls", "l": 12, "n": r'^(?P<page>\d+)/$', "c": [md5("test_project.films.views:123")]},
+        {"i": md5("test_project.films.views:123"), "t": "View", "f": "test_project.films.views", "l": 123, "n": "get", "c": [md5("test_project.films.forms:23")]},
+        {"i": md5("test_project.films.models:42"), "t": "Model", "f": "test_project.films.models", "l": 42, "n": "__unicode__"},
+        {"i": md5("test_project.films.forms:23"), "t": "Form", "f": "test_project.films.forms", "l": 23, "n": "init", "c": [md5("test_project.films.models:42")]},
+        {"i": md5("test_project.films.forms:31"), "t": "Form", "f": "test_project.films.forms", "l": 31, "n": "clean", "c": [md5("test_project.films.models:42")]},
+    ]
+    Trace.objects.create(
+        session=session,
+        json=simplejson.dumps(dump))
--- test_project/test_project/films/views.py
+++ test_project/test_project/films/views.py
@@ -3,9 +3,5 @@ from .models import Film
 
 
 class MainPageView(ListView):
-    paginate_by = 10
     queryset = Film.objects.order_by('-pk')
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(MainPageView, self).get_context_data(**kwargs)
-    #     return context
+    paginate_by = 10
